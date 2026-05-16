466e43578603cbc61b375d617b45b513573e71c9
SAnzil107 <be2022se826@gces.edu.np>
2026-05-16 23:04:58 +0545

    chapter 2 practice set done

---
+++ src/com/company/CH2_practiceSet.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.util.Scanner;
+
+public class CH2_practiceSet {
+    static void main(String[] args) {
+        //Exercise1
+//        float a = 7/4 * 9/2;//outputs 4
+        float a = 7/4.0f * 9/2.0f;//outputs 7.875
+        System.out.println(a);
+
+        //Exercise2
+
+//        char b = 'B';
+//        char grade = (char)(b+8);
+//        System.out.println( grade);
+//        System.out.println((char) (grade-8));
+
+        //Exercise 3
+//        int a=56;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        int b = sc.nextInt();
+//        System.out.println(a>b);
+
+        //Exercise 4
+//        float v=10;
+//        float u =5;
+//        float s=2;
+//        float a= 3;
+//        float calculation = (v*v - u*u)/ (2*a*s);
+//        System.out.println(calculation);
+    }
+}
