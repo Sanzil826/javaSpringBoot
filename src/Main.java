60182d380a799b6443b94a942d346618e96bf82a
SAnzil107 <be2022se826@gces.edu.np>
2026-05-16 21:02:42 +0545

    first program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        int num1 = 6;
+        int num2 = 8;
+        int sum = num1+ num2;
+        System.out.println(sum);
+    }
+}
