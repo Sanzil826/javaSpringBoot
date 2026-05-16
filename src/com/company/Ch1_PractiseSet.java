9407955936be3d5e7e8499d2ebcbb77b61804ee7
SAnzil107 <be2022se826@gces.edu.np>
2026-05-16 22:04:05 +0545

    chapter 1 practise set

---
+++ src/com/company/Ch1_PractiseSet.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+
+public class Ch1_PractiseSet {
+    static void main(String[] args) {
+        //Exercise 1
+        System.out.println("Enter your name:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + ", have a good day");
+
+        //Exercise 2
+//        System.out.println("Enter distance in kilometer:");
+//          Scanner sc = new Scanner(System.in);
+//        double km = sc.nextFloat();
+//        double mile = km* 0.621371;
+//        System.out.println(km+" in mile is: "+mile);
+
+        //Exercise 3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        System.out.println(sc.hasNextInt());
+//    }
+    }
+}
