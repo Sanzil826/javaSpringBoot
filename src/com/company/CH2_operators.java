c1856b1a2a061dec227d0921f730965fa9b07eb9
SAnzil107 <be2022se826@gces.edu.np>
2026-05-16 22:25:00 +0545

    types of operators

---
+++ src/com/company/CH2_operators.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CH2_operators {
+    static void main(String[] args) {
+        int a =4;
+        int b = 6+a;
+        System.out.println(b);
+        System.out.println(6==6);
+        System.out.println(64>5 && 64>8); //true if both condition are true
+        System.out.println(64>5 || 78>77);// true if one of the condition is true
+        System.out.println(2&3);
+    }
+}
