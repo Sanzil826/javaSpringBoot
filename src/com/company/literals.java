0c62439abf85b3baa2080d3e2b04a0c98c242e10
SAnzil107 <be2022se826@gces.edu.np>
2026-05-16 21:14:30 +0545

    learned literals

---
+++ src/com/company/literals.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class literals {
+    public static void main(String[] args) {
+        int age = 45;
+        char ch ='A';
+        float f1 = 5.6f;
+        double d1= 4.66D;
+        boolean a = true;
+        System.out.println(age);
+    }
+}
