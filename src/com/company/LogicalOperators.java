7e37eafff88ecdec2e404ff931f343aac2259ff4
SAnzil107 <be2022se826@gces.edu.np>
2026-05-17 11:37:18 +0545

    logical operators

---
+++ src/com/company/LogicalOperators.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class LogicalOperators {
+    static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+//        if (a || b) {
+//            System.out.println("YEs");
+//        }else
+//            System.out.println("No");
+//        }
+
+        if (a && !b) {
+            System.out.println("YEs");
+        } else
+            System.out.println("No");
+    }
+}
+
+
