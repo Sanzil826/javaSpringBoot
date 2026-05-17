f975d41cd70935252917e005ce136a931c35fb06
SAnzil107 <be2022se826@gces.edu.np>
2026-05-17 11:30:00 +0545

    learned basic if statement

---
+++ src/com/company/IF_else.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class IF_else {
+    static void main(String[] args) {
+        int a = 18;
+        if (a>=18) {
+            System.out.println("You can drive");
+        }
+            else{
+                System.out.println("You cannot drive");
+            }
+        }
+    }
+
