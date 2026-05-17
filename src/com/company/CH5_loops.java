c6fd82236eb56c7a87b877f65b700f5219ed96a7
SAnzil107 <be2022se826@gces.edu.np>
2026-05-17 22:16:50 +0545

    learned about loops

---
+++ src/com/company/CH5_loops.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Scanner;
+
+public class CH5_loops {
+    static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int num = sc.nextInt();
+//        int i=1;
+//        int j=0;
+
+//        while (i<=3){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        do{
+//            System.out.println("I am do while");
+//            i++;
+//        }while(i<1); // do while always run at least once
+
+//        for(i=1; i<=2*num +1; i=i+2){
+//            System.out.println(i);
+//        }
+
+//        for (j=0;j<num;j++){
+//            System.out.println(2*j+1);
+//        }
+
+        for (int k=5; k>=0;k--){
+            System.out.println(k);
+        }
+    }
+}
