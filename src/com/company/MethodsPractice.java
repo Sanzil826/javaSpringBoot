92ecb19d2bb014f688570e113cfc65ba0f543ec1
SAnzil107 <be2022se826@gces.edu.np>
2026-05-18 22:25:27 +0545

    PracticeSet on methods

---
+++ src/com/company/MethodsPractice.java
@@ -0,0 +1,92 @@
+package com.company;
+
+import java.util.Scanner;
+//import java.lang.String;
+
+
+public class MethodsPractice {
+    //Exercise1
+//    static void multiplicationTable(int n){
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(n+" X "+i+" = "+n*i);
+//        }
+//    }
+
+    //Exercise2
+//    static void pattern(){
+//        for (int i = 1; i <=4 ; i++) {
+//            for (int j = 1; j <=i ; j++) {
+//                System.out.print("*\t");
+//            }
+//            System.out.println("\n");
+//        }
+//    }
+
+    //Exercise3
+//    static int sum(int n) {
+//        if (n == 1) {
+//            return 1;
+//        } else {
+//            return n + sum(n - 1);
+//        }
+//    }
+
+    //Exercise4
+//    static void pattern1(){
+//        for (int i = 1; i <=4 ; i++) {
+//            for (int j = i; j <=4 ; j++) {
+//                System.out.print("*\t");
+//            }
+//            System.out.println("\n");
+//        }
+//    }
+
+    //Exercise5
+//    static int fibonacci(int n) {
+//        if (n == 1 || n == 2)
+//            return 1;
+//        else {
+//            return fibonacci(n-1) + fibonacci(n - 2);
+//        }
+//    }
+
+    //Exercise6
+//    static int average(int ...arr){
+//        int sum = 0;
+//        for (int elem: arr) {
+//            sum = sum + elem;
+//        }
+//        return sum/(arr.length);
+//
+//    }
+
+
+    static void main(String[] args) {
+        //Exercise1
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int num = s.nextInt();
+//        multiplicationTable(num);
+
+        //Exercise2
+//        pattern();
+
+        //Exercise3
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int num = s.nextInt();
+//        System.out.println(sum(num));
+
+        //Exercise4
+//        pattern1();
+
+        //Exercise5
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Enter a number: ");
+//        int num = s.nextInt();
+//        System.out.println(fibonacci(num));
+
+        //Exercise6
+//        System.out.println(average(7,7,7,7,7));
+    }
+}
